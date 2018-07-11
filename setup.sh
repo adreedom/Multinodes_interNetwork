8c370e5719da27946973619cc13862a434a688e3
adreedom <ziyu_jules@qq.com>
2018-07-11 19:17:21 +0800

    test

---
+++ setup.sh
@@ -0,0 +1,79 @@
+docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}"
+
+cryptogen generate --config=./crypto-config.yaml
+
+
+export FABRIC_CFG_PATH=$PWD
+configtxgen -profile MultiOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile MultiOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID 'mychannel'
+configtxgen -profile MultiOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID 'mychannel' -asOrg Org1MSP
+configtxgen -profile MultiOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID 'mychannel' -asOrg Org2MSP
+configtxgen -profile MultiOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID 'mychannel' -asOrg Org3MSP
+
+export CLI_TIMEOUT=10
+#default for delay
+export CLI_DELAY=3
+# channel name defaults to "mychannel"
+export CHANNEL_NAME="mychannel"
+# use this as the default docker-compose yaml definition
+export COMPOSE_FILE=docker-compose-cli.yaml
+#
+export COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+
+docker-compose -f docker-compose-cli.yaml up peer3.org1.example.com peer2.org2.example.com peer4.org1.example.com peer0.org1.example.com
+docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml -f docker-compose-e2e.yaml up
+
+CHANNEL_NAME=$CHANNEL_NAME TIMEOUT=$CLI_TIMEOUT DELAY=$CLI_DELAY docker-compose -f docker-compose-cli.yaml up 
+#-d 2>&1
+
+
+export CHANNEL_NAME="mychannel"
+export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls true --cafile $ORDERER_CA
+
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx
+
+CORE_PEER_LOCALMSPID="Org1MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+
+peer channel join -b $CHANNEL_NAME.block 
+
+CORE_PEER_LOCALMSPID="Org2MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=peer0.org2.example.com:7051
+
+CORE_PEER_LOCALMSPID="Org3MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+CORE_PEER_ADDRESS=peer0.org3.example.com:7051
+
+
+# update Anchor Peers
+peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls true --cafile $ORDERER_CA 
+
+#>&log.txt
+
+
+# install chaincode
+peer chaincode install -n Bchain -v 1.0 -l node -p '/opt/gopath/src/github.com/chaincode/Bchain/node/'
+peer chaincode install -n Bchain -v 1.0 -l golang -p 'github.com/chaincode/Bchain/go/'
+
+# instantiate Chaincode
+peer chaincode instantiate -o orderer.example.com:7050 --tls true --cafile $ORDERER_CA -C $CHANNEL_NAME -n Bchain -l golang -v 1.0 -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member','Org3MSP.member')"
+
+peer chaincode invoke -o orderer.example.com:7050  --tls true --cafile $ORDERER_CA -C $CHANNEL_NAME -n Bchain -c '{"Args":["initLedger"]}'
+
+docker rm -f $(docker ps -aq)
+docker network prune
+
+CHANNEL_NAME="mychannel"
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050  --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n Bchain -c '{"Args":["queryAllAsset",""]}'
+
+
+
+
+
